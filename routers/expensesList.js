2f4ab000b6ca9dd98822f3c432a457badf279e15
raghava20 <raghavaanand22@gmail.com>
2022-01-20 16:07:43 +0530

    connected mongodb along routes

--- routers/expensesList.js
+++ routers/expensesList.js
@@ -1,10 +1,22 @@
 import express from "express";
+import { Expenses } from "../models/Expenses.js";
 
 const router = express.Router();
 
 router.route("/").get(async (req, res) => {
-    res.send({})
+    try {
+        var response = await Expenses.find();
+        res.send(response)
+    }
+    catch (err) {
+        res.send(err)
+    }
 
+})
+router.route("/:id").delete(async (req, res) => {
+    const { id } = req.params;
+    var response = await Expenses.findByIdAndRemove(id)
+    res.send(response)
 })
 
 export const expensesListRouter = router;
